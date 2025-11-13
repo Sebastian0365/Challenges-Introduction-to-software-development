a3cd917df3bd5b5b2f68a9b7b031069e93fee253
Sebastian0365 <227214393+Sebastian0365@users.noreply.github.com>
2025-11-12 20:55:57 -0500

    Create challenges-01.py
    1. Challenge (Lanzar dado y determinar si es par o impar)

---
+++ challenges-01.py
@@ -0,0 +1,31 @@
+import random
+
+#Lanzar dado
+numero=random.randint(1,6)
+
+print ("El numero que salio es: ", numero)
+
+#Ver si es par o impar
+
+if numero == 1:
+    print (numero, "Es Impar")
+
+
+if numero == 2:
+    print (numero, "Es Par")
+
+
+if numero == 3:
+    print (numero, "Es Impar")
+
+
+if numero == 4:
+    print (numero, "Es Par")
+
+
+if numero == 5:
+    print (numero, "Es Impar")
+
+
+if numero == 6:
+    print (numero,"Es Par")
