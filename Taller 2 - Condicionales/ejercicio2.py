40b9faf4bb6d4687d4322cf115e6492859d69054
Sebastian0365 <227214393+Sebastian0365@users.noreply.github.com>
2025-11-24 21:14:15 -0500

    Reto condicional 2
    Se crea archivo ,py con el desarrollo del ejercicio numero 2, en el que se hace un programa que imprima un numero aleatorio y valide si el numero es par o impar

---
+++ Taller 2 - Condicionales/ejercicio2.py
@@ -0,0 +1,11 @@
+numero=int(input("Ingrese un numero entero: "))
+
+print("El numero ingresado es:", numero)
+
+if numero % 2 == 0:
+    print("El numero es par.")
+
+else:
+    print("El numero es impar.")
+
+    
